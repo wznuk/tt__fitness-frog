c834ecba11f73aa73718adc4fc9e6b1c74980fd2
wznuk <westonkunz@gmail.com>
2018-03-16 08:53:46 -0500

    Added validation summery section

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -55,6 +55,7 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+
             if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
             {
                 ModelState.AddModelError("Duration", "The Duration field value must be greater than 0.");
